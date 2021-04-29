1ee858db7abd8acfffd8ca0a33941a884bf45f3d
ishanshrimali <ishanshrimali@chicagobooth.edu>
2021-04-29 01:33:19 +0000

    completed comment author authentications

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
+  before_action :ensure_current_user_is_author, only: [:edit, :update, :destroy]
 
   # GET /comments or /comments.json
   def index
@@ -61,6 +62,12 @@ class CommentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
+    end
+
+    def ensure_current_user_is_author
+      if current_user != @comment.author
+        redirect_back fallback_location: root_url, alert: "That isn't your comment."
+      end
     end
 
     # Only allow a list of trusted parameters through.
